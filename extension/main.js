f8ffebaf6bc5e58ca88ecc07d1dbc7deeef4355c
svento <mail@sven.to>
2013-11-03 15:23:47 +0100

    working

--- extension/main.js
+++ extension/main.js
@@ -1,4 +1,1 @@
 $("body").append("<div id=\"chrome-retro-overlay\"></div>");
-
-$( "img" ).wrap( "<div class='chrome-retro-img-container'></div>" );
-$( ".chrome-retro-img-container" ).append( "<div class='chrome-retro-img-overlay'></div>" );
