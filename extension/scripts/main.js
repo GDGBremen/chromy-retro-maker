7b8a16d9860e96929000d541185151af79c0b55c
Steve Liedtke <sliedtke57@gmail.com>
2013-11-03 16:54:11 +0100

    added great game donkey to extension

--- extension/scripts/main.js
+++ extension/scripts/main.js
@@ -1,2 +1,14 @@
+var gameOpened = false;
+
 $("body").append("<div id=\"chrome-retro-overlay\"></div>");
 $("body").append("<div id=\"chrome-retro-game-link\" alt=\"Game\">"); // TODO:.onclick();
+
+$("#chrome-retro-game-link").click(function(){
+	if(gameOpened){
+		// remove it from website
+	}else{
+		$("body").append("<canvas id=\"donkeyCanvas\" width=\"600\" height=\"375\"></canvas>");
+		document.onkeypress = handleKeyboardEvent;
+		redraw(0);
+	}
+});
